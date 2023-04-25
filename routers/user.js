c82b7621d9d25d1f5da6baad2275304cf4eb3721
bodyavl <88792596+bodyavl@users.noreply.github.com>
2023-04-25 11:11:59 +0300

    test

--- routers/user.js
+++ routers/user.js
@@ -84,6 +84,7 @@ router.delete('/logout', (req, res, next) => {
 function authToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader.split(' ')[1];
+    console.log(token);
     if(!token) return res.sendStatus(401);
   
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
