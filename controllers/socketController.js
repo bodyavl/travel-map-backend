9cee43fb5dbe93fcf0ecf36f613a2e1dffbbe46f
Bohdan Vlasenko <bodyavl228@gmail.com>
2023-06-23 12:15:38 +0300

    error resolved

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Mark = require('../database/models/mark')
 
 function socketController(socket) {
-    constole.log(`User ${socket.id} connected`)
+    console.log(`User ${socket.id} connected`)
     socket.on("new marker", async (id) => {
         const marker = await Mark.findById(id);
         if(marker) socket.broadcast.emit('fetch new', marker)
